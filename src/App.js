f9c786aae830d1326f0927f35c464c9549758bb3
Sophie <sophiekbodnar@gmail.com>
2022-02-17 22:05:28 -0500

    cleaning up some uneeded code and console logs

--- src/App.js
+++ src/App.js
@@ -1,74 +1,69 @@
 import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "./App.css";
-// import { Icon } from "leaflet";
 import data from "./data/data.json";
+import Header from "./components/Header";
 
-//---------------------------------Checking to see if data is coming in
-// console.log(data);
 
 function App() {
-  // Managing state for Trucks onClick
+  // Managing state for Trucks to track if "clicked" or not
   const [activeTruck, setActiveTruck] = useState(null);
 
   return (
     <div className="App">
-      <MapContainer
-        className="leaflet-container"
-        //  Starting point when page renders
-        center={[37.77949545478787, -122.41927388849264]}
-        zoom={13}
-        scrollWheelZoom={false}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {/* TO GET THE MAKERS: Map over the data and give the marker a unique key as well as the coordinates of the food truck  */}
-        {data.trucks.map((truck) => (
-          <Marker
-            key={truck.id}
-            position={truck.coordinates}
-            eventHandlers={{
-              click: () => {
-                setActiveTruck(truck);
-                console.log("onClick works");
-              },
-            }}
+      <div className="page-container">
+        <MapContainer
+          className="leaflet-container"
+          //  Starting point when page renders
+          center={[37.77949545478787, -122.41927388849264]}
+          zoom={13}
+          scrollWheelZoom={false}
+        >
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          />
 
-            // onClick={() => {
-            //   // updating state
-            //   setActiveTruck(truck);
-            //   console.log("onClick works!")
-            // }}
-          ></Marker>
-        ))}
-        {console.log("this is activeT", activeTruck)}
-        {activeTruck && (
-          <Popup
-            position={activeTruck.coordinates}
-            onClose={() => {
-              setActiveTruck(null);
-            }}
-          >
-            <div>
-              <h2>
-                <b>{activeTruck.name}</b>
-              </h2>
-              <p>
-                <b>Food Types:</b>
-                <br />
-                {activeTruck.description}
-              </p>
-              <p>
-                <b>Address:</b>
-                <br />
-                {activeTruck.address}
-              </p>
-            </div>
-          </Popup>
-        )}
-      </MapContainer>
+          {/* TO GET THE MARKERS: Map over the data and give the marker a unique key as well as the coordinates of the food truck  */}
+          {data.trucks.map((truck) => (
+            <Marker
+              key={truck.id}
+              position={truck.coordinates}
+              // handle user click and update state
+              eventHandlers={{
+                click: () => {
+                  setActiveTruck(truck);
+                },
+              }}
+            ></Marker>
+          ))}
+          {activeTruck && (
+            <Popup
+              position={activeTruck.coordinates}
+              onClose={() => {
+                setActiveTruck(null);
+              }}
+            >
+              <div>
+                <h2>
+                  <b>{activeTruck.name}</b>
+                </h2>
+                <p>
+                  <b>Food Types:</b>
+                  <br />
+                  {activeTruck.description}
+                </p>
+                <p>
+                  <b>Address:</b>
+                  <br />
+                  {activeTruck.address}
+                </p>
+              </div>
+            </Popup>
+          )}
+        </MapContainer>
+        <Header />
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+ import "../App.css";
+
+
+export default function Header () {
+
+
+    return (
+        <div>
+          <h1 className="header">That Food Truck Habit</h1>  
+        </div>
+    )
+}
